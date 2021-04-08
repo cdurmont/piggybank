47adc984c64020f69398315b50bf1141444c510d
Cédric Durmont <cedric@durmont.net>
2021-04-08 09:59:11 +0200

    PIG-2 CRUD transactions transactions only (entries in transactions ignored)

--- controllers/transactionController.ts
+++ controllers/transactionController.ts
@@ -1,25 +1,66 @@
-import Transaction from "../models/transaction";
 import ITransaction from "../models/ITransaction";
 import TransactionService from "../services/transactionService";
 import {Request, Response} from "express";
+import validator from "../config/validator";
 
 const TransactionController = {
     create: (req: Request, res: Response) => {
-        let trans:ITransaction = {
-            balanced: req.body.balanced
-        };
+        if (!validator.getSchema<ITransaction>('transaction')(req.body))
+            return res.status(400).json({error: 'Invalid Transaction JSON'});
+        let trans:ITransaction = req.body;
+
+        TransactionService.create(trans, (err, trans) => {
+            if (err) {
+                console.error('Error creating transaction ' + trans);
+                return res.status(400).json({error: 'Error creating transaction', detail: err});
+            }
+            res.json(trans);
+        })
     },
 
     read: (req: Request, res: Response) => {
+        if (!validator.getSchema<ITransaction>('transaction')(req.body))
+            return res.status(400).json({error: 'Invalid Transaction JSON'});
+        let trans:ITransaction = req.body;
 
+        TransactionService.read(trans, (err, transList) => {
+            if (err) {
+                console.error('Error reading transactions, filter= ' + trans);
+                return res.status(400).json({error: 'Error reading transaction', detail: err});
+            }
+            res.json(transList);
+        })
     },
 
     update: (req: Request, res: Response) => {
+        if (!validator.getSchema<ITransaction>('transaction')(req.body))
+            return res.status(400).json({error: 'Invalid Transaction JSON'});
+        let trans:ITransaction = req.body;
 
+        if (!req.params.id)
+            return res.status(404).json({error: 'no transaction id specified'});
+        trans._id = req.params.id;
+
+        TransactionService.update(trans, (err, trans) => {
+            if (err) {
+                console.error('Error updating transaction ' + trans);
+                return res.status(400).json({error: 'Error updating transaction', detail: err});
+            }
+            res.status(200).end();
+        })
     },
 
     delete: (req: Request, res: Response) => {
-
+        if (!req.params.id)
+            return res.status(404).json({error: 'no transaction id specified'});
+        let trans:ITransaction = { _id: req.params.id};
+        TransactionService.delete(trans, err => {
+            if (err) {
+                console.error('Error deleting transaction ' + trans);
+                return res.status(400).json({error: 'Error deleting transaction', detail: err});
+            }
+            res.status(200).end();
+        })
     }
 };
 
